86861256f8ed21c30a53c07ab06936fed689a888
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-05-11 06:58:37 -0500

    Archivos de la página web Rochi Kahn 2016

---
+++ js/miniatura-slider.js
@@ -0,0 +1,41 @@
+//======== ALMACENANDO VARIABLES (SLIDER Y BOTONES) ==========//
+
+var slider2 = $('#slider2');
+var avanzar2 = $('#btn-next2');
+var retroceder2 = $('#btn-prev2');
+
+//===========  INSERTANDO LA ULTIMA IMAGEN DEL SLIDER AL PRINCIPIO DEL RECORRIDO  ===========//
+$('#slider2 section:last').insertBefore('#slider2 section:first');
+
+//===== 	MOVIENDO EL MARGEN PARA VER LA PRIMERA IMAGEN    ==============//
+slider2.css('margin-left', '-'+100+'%');
+
+//========	PROGRAMANDO EL BOTON DE AVANZAR   ===========//
+function moverD2(){
+	slider2.animate({marginLeft:'-'+200+'%'},700, function(){
+		//==  INSERTANDO LA PRIMERA IMAGEN DEL SLIDER AL FINAL DEL RECORRIDO  ==//
+		$('#slider2 section:first').insertAfter('#slider2 section:last');
+		//=== MOVIENDO EL MARGEN PARA VER LA IMAGEN CORRECTA//
+		slider2.css('margin-left', '-'+100+'%');
+	});
+}
+
+//========	PROGRAMANDO EL BOTON DE REGRESAR   ===========//
+function moverI2(){
+	slider2.animate({marginLeft:0},700, function(){
+		//==  INSERTANDO LA ULTIMA IMAGEN DEL SLIDER AL INICIO DEL RECORRIDO  ==//
+		$('#slider2 section:last').insertBefore('#slider2 section:first');
+		//=== MOVIENDO EL MARGEN PARA VER LA IMAGEN CORRECTA//
+		slider2.css('margin-left', '-'+100+'%');
+	});
+}
+
+//=== ACTIVANDO BOTON DE AVANZAR ===//
+avanzar2.on('click',function(){
+	moverD2();
+});
+
+//=== ACTIVANDO BOTON DE RETROCEDER ===//
+retroceder2.on('click', function(){
+	moverI2();
+});
