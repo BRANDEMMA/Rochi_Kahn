b7e2b65e72d94e3bbed64405c85917da55d411f6
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-06-10 08:54:56 -0500

    texto cuidados y tiempo elaboracion

--- baul-bolas-r.php
+++ baul-bolas-r.php
@@ -187,25 +187,9 @@
 					</div> 
 				</div>
 				<div class="texto-articulo">
-					<div class="boton-mas-detalles" id="mas-cuidados"><a id="mas-detalles"><strong>Cuidados del Producto</strong> &nbsp;&nbsp;&nbsp;&nbsp;<img src="img/prensa/ver-mas.gif"></a></div>
-					<div style="display:none;" class="boton-mas-detalles" id="menos-cuidados"><a id="menos-detalles"><strong>Cuidados del Producto</strong> &nbsp;&nbsp;&nbsp;&nbsp;<img src="img/prensa/ver-menos.gif"></a></div>
-					<p style="display:none;" class="contenido-mostrar" id="cuidados-productos">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices mollis arcu non condimentum. Duis ex nisi, tristique sit amet mollis et, accumsan vel enim. Praesent iaculis ex ac ipsum viverra, et pellentesque libero varius. Proin porta id magna a elementum. Fusce condimentum quam eget vestibulum dictum. Ut tortor quam, consectetur et lacus sed, rhoncus sollicitudin erat.<br><br>
-
-					Aliquam elementum elit et quam lobortis, vitae molestie neque rutrum. Morbi vitae venenatis est, sed hendrerit ante. In hac habitasse platea dictumst. Suspendisse tincidunt odio ligula. In tincidunt sapien faucibus luctus commodo. Donec bibendum dui elit, vel maximus mauris fermentum a.<br><br>
-
-					Aliquam erat volutpat. Morbi venenatis luctus convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed laoreet leo. Fusce lobortis vehicula interdum. Donec placerat finibus lacinia. Aliquam scelerisque a dolor sit amet ornare.</p>
-
-
-					<div class="boton-mas-detalles" id="mas-elaboracion"><a id="mas-detalles2"><strong>Tiempo de elaboración</strong> &nbsp;&nbsp;&nbsp;&nbsp;<img src="img/prensa/ver-mas.gif"></a></div>
-					<div style="display:none;" class="boton-mas-detalles" id="menos-elaboracion"><a id="menos-detalles2"><strong>Tiempo de elaboración</strong> &nbsp;&nbsp;&nbsp;&nbsp;<img src="img/prensa/ver-menos.gif"></a></div>
-					<p style="display:none;" class="contenido-mostrar" id="tiempo-elaboracion">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices mollis arcu non condimentum. Duis ex nisi, tristique sit amet mollis et, accumsan vel enim. Praesent iaculis ex ac ipsum viverra, et pellentesque libero varius. Proin porta id magna a elementum. Fusce condimentum quam eget vestibulum dictum. Ut tortor quam, consectetur et lacus sed, rhoncus sollicitudin erat.<br><br>
-
-					Aliquam elementum elit et quam lobortis, vitae molestie neque rutrum. Morbi vitae venenatis est, sed hendrerit ante. In hac habitasse platea dictumst. Suspendisse tincidunt odio ligula. In tincidunt sapien faucibus luctus commodo. Donec bibendum dui elit, vel maximus mauris fermentum a.<br><br>
-
-					Aliquam erat volutpat. Morbi venenatis luctus convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed laoreet leo. Fusce lobortis vehicula interdum. Donec placerat finibus lacinia. Aliquam scelerisque a dolor sit amet ornare.</p>
-
-
-					<br><br>
+					<?php
+						include 'cuidados-tiempo.php';
+					?>	
 					<a href=""><i class="fa fa-mobile"></i>(511) 985 047 810</a>
 				</div>
 				<div class="formulario-articulo" id="solicitar">
